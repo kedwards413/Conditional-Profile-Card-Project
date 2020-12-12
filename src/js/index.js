1101dba53c2e81397fd448adb6fd7e9b5a369bb4
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-12 00:36:38 +0000

    updates

--- src/js/index.js
+++ src/js/index.js
@@ -33,9 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name} ${variables.lastname}</h1> 
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
@@ -56,19 +56,20 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://media-exp1.licdn.com/dms/image/C5103AQHtmq7UtMeUSw/profile-displayphoto-shrink_400_400/0/1517555840267?e=1613001600&v=beta&t=oB6_L44BGIb-wiGVB9sWUjTzY1g1qKp_zzGaeyIk5Jg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
-    github: "alesanchezr",
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    twitter: "Twitter",
+    github: "Github",
+    linkedin: "LinkedIn",
+    instagram: "Instagram",
+    name: "Name",
+    lastname: "",
+    role: "Role",
+    country: "Country",
+    city: "City"
   };
   render(window.variables); //render the card for the first time
 

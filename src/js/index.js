099a896faeba37d7453a1783cd7e8ed767808211
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-12 01:32:45 +0000

    updates to js

--- src/js/index.js
+++ src/js/index.js
@@ -31,16 +31,16 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
+            ${variables.background}
+          <img src="${variables.avatar}" class="photo" />
           <h1>${variables.name} ${variables.lastname}</h1> 
           <h2>${variables.role}</h2>
           <h3>${variables.city} ${variables.country}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/${variables.twitter} target="_blank"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github} target="_blank"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin} target="_blank"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram} target="_blank"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -69,7 +69,9 @@ window.onload = function() {
     lastname: "",
     role: "Role",
     country: "Country",
-    city: "City"
+    city: "City",
+    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    avatar: "https://media-exp1.licdn.com/dms/image/C5103AQHtmq7UtMeUSw/profile-displayphoto-shrink_400_400/0/1517555840267?e=1613001600&v=beta&t=oB6_L44BGIb-wiGVB9sWUjTzY1g1qKp_zzGaeyIk5Jg"
   };
   render(window.variables); //render the card for the first time
 
